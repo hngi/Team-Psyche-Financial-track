ed8ef99571738735942cdb0d50a85b3f2630d198
kaperskyguru <solomoneseme@gmail.com>
2019-09-28 15:47:57 +0100

    Protecting routes

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,19 @@
  */
 
 Route::group(['prefix' => 'v1'], function () {
+
     Route::post('login', 'UsersController@login')->name('login');
     Route::post('register', 'UsersController@signup')->name('register');
-    Route::post('logout', 'UsersController@logout')->name('logout');
-    Route::get('verify/{token}', 'UsersController@verify')->name('verify');
-    Route::get('users', 'UsersController@all')->name('users');
-    Route::get('users/{id}', 'UsersController@find')->name('users');
-    Route::get('items/info', 'ItemsController@counts');
-    Route::get('items/weekly', 'ItemsController@showWeeklyItems')->name('weekly');
-    Route::get('items/monthly/{month?}', 'ItemsController@showMonthlyItems')->name('monthly');
-    Route::get('items/yearly/{year?}', 'ItemsController@showYearlyItems')->name('yearly');
-    Route::resource('items', 'ItemsController');
     Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('logout', 'UsersController@logout')->name('logout');
+        Route::get('verify/{token}', 'UsersController@verify')->name('verify');
+        Route::get('users', 'UsersController@all')->name('users');
+        Route::get('users/{id}', 'UsersController@find')->name('users');
+        Route::get('items/info', 'ItemsController@counts');
+        Route::get('items/weekly', 'ItemsController@showWeeklyItems')->name('weekly');
+        Route::get('items/monthly/{month?}', 'ItemsController@showMonthlyItems')->name('monthly');
+        Route::get('items/yearly/{year?}', 'ItemsController@showYearlyItems')->name('yearly');
+        Route::resource('items', 'ItemsController');
 
     });
 
